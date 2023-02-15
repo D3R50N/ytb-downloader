cf07396ecebd31374ac19977f031fcd3bdfd560c
Max Anderson MOUMOUNI <happydev.229@gmail.com>
2023-02-15 10:47:34 +0100

    Some updates

--- content.js
+++ content.js
@@ -1,23 +1,27 @@
 /**
  * @returns l'onglet actif de chrome
-*/
+ */
 async function getCurrentTab() {
-    let queryOptions = {
-        active: true,
-        lastFocusedWindow: true
-    };
+  let queryOptions = {
+    active: true,
+    lastFocusedWindow: true,
+  };
 
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
-
-
-
-getCurrentTab().then((t) => {
-    tab = t;
-    base = tab.url.split("/");
-    base.pop();
-    base = base.join("/");
+getCurrentTab().then((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: open_y2mate,
+    });
 });
 
-btn.addEventListener('click', getFiles); // au clic du bouton
+function open_y2mate() {
+  var id = window.location.href.split("=")[1];
+  var url = "https://y2mate.com/youtube/" + id;
+  window.open(url, "_blank");
+  
+}
+
+dEventListener("click", getFiles); // au clic du bouton
